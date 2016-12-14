6d5b8898ae8a21d4eb1e20ebb963981c2ed93c26
Jens Schulze <jens.schulze@commercetools.de>
2016-12-14 11:57:56 +0100

    add custom object with customer number implementation

--- src/Commercetools/TrainingBundle/Service/CustomObjectRepository.php
+++ src/Commercetools/TrainingBundle/Service/CustomObjectRepository.php
@@ -27,6 +27,14 @@ class CustomObjectRepository
      */
     public function store($container, $key, $value, $version = null)
     {
+        $object = CustomObjectDraft::ofContainerKeyAndValue($container, $key, $value);
+        if (!is_null($version)) {
+            $object->setVersion($version);
+        }
+        $request = CustomObjectCreateRequest::ofObject($object);
+        $response = $this->client->execute($request);
+
+        return $request->mapFromResponse($response);
     }
 
     /**
@@ -34,7 +42,15 @@ class CustomObjectRepository
      */
     public function getCustomerNumberObject()
     {
-        return null;
+        $request = CustomObjectByKeyGetRequest::ofContainerAndKey('stuff', 'customerNumber');
+        $response = $this->client->execute($request);
+        $customObject = $request->mapFromResponse($response);
+
+        if (is_null($customObject)) {
+            $customObject = CustomObjectDraft::ofContainerKeyAndValue('stuff', 'customerNumber', 0);
+        }
+
+        return $customObject;
     }
 
     /**
@@ -46,6 +62,29 @@ class CustomObjectRepository
         if (is_null($customObject)) {
             $customObject = $this->getCustomerNumberObject();
         }
+
+        $newCustomerNumber = $customObject->getValue() + 1;
+        $customObject->setValue($newCustomerNumber);
+
+        $request = CustomObjectCreateRequest::ofObject($customObject);
+        $response = $this->client->execute($request);
+
+        $retries = 0;
+        while ($retries < 5 && $response->isError()) {
+            $error = $response->getErrors()->getByCode(ConcurrentModificationError::CODE);
+            if ($error instanceof ConcurrentModificationError) {
+                $customObject = $this->getCustomerNumberObject();
+                $newCustomerNumber = $customObject->getValue() + 1;
+                $customObject->setValue($newCustomerNumber);
+                usleep(10000);
+                $request = CustomObjectCreateRequest::ofObject($customObject);
+                $response = $this->client->execute($request);
+            } else {
+                throw new \Exception('something happened');
+            }
+        }
+
+        $customObject = $request->mapFromResponse($response);
 
         return $customObject->getValue();
     }
